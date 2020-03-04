1f7e32cc0abcc86170954c0ea2f294aae4ca80a1
Алексей Мороз <alexeymoroz@Alex-Moroz.local>
2020-03-04 12:06:53 +0200

    Updates

---
+++ precache-manifest.63873872314c6e6250052ed9a0002988.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26aaa213b61ce5348ff8822158a84188",
+    "url": "/goit-react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "fdd3187923daa39480f5",
+    "url": "/goit-react-hw-02-phonebook/static/css/main.50c923d0.chunk.css"
+  },
+  {
+    "revision": "8062e0f739bc81da398e",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d8d0a24a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-02-phonebook/static/js/2.d8d0a24a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fdd3187923daa39480f5",
+    "url": "/goit-react-hw-02-phonebook/static/js/main.1b04e5ff.chunk.js"
+  },
+  {
+    "revision": "c3f8367271860c7b9688",
+    "url": "/goit-react-hw-02-phonebook/static/js/runtime-main.cab97b21.js"
+  }
+]);
